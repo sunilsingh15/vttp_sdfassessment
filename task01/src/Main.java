fb76e252c63b937217db5e92531388e52e5a1d1d
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-05-12 15:35:07 +0800

    completed task 2 and added comments for both files for understanding

--- task01/src/Main.java
+++ task01/src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // following assessment sample output
         System.out.println("Welcome.");
 
+        // new scanner object to accept input from user inside while loop
         Scanner scan = new Scanner(System.in);
         String input = "";
 
+        // declaration of both numbers in expression
+        // also result, and last variable for use later
         double num1 = 0.0;
         double num2 = 0.0;
         double result = 0.0;
@@ -15,18 +19,31 @@ public class Main {
 
         while (!input.equals("exit")) {
             System.out.print("> ");
+            // accept input from user after prompt arrow
             input = scan.nextLine();
 
+            // break out of loop if user enters exit, will go to after while loop which will
+            // print "Bye bye"
             if (input.equals("exit")) {
                 break;
             }
 
+            // split input into a string array to analyze each
+            // index and perform tasks accordingly
             String[] expression = input.split(" ");
 
+            // if there are less than 3 terms in expression
+            // reject input
             if (expression.length != 3) {
                 System.out.println("Expression entered is not valid. Try again.");
                 continue;
             }
+
+            // two if statements to check if first or last input entered
+            // is equal to $last
+            // if so, assign last variable to num1/num2
+            // otherwise, convert from string array to a double
+            // to perform math operations
 
             if (expression[0].equals("$last")) {
                 num1 = last;
@@ -39,6 +56,11 @@ public class Main {
             } else {
                 num2 = Double.parseDouble(expression[2]);
             }
+
+            // switch statement
+            // will check what operator user gives
+            // and go into individual case accordingly
+            // and perform operation
 
             switch (expression[1]) {
                 case "+":
@@ -58,11 +80,19 @@ public class Main {
                     continue;
             }
 
+            // once breaks from switch statement, will come here
+            // this will print the result for user like sample output
+            // and save result value to last variable
+            // so we can use $last in future
             System.out.println(result);
             last = result;
 
         }
 
+        // end of program
+        // close resources (Scanner)
+        // print message as shown in sample output
+        // shut program down
         scan.close();
         System.out.println("Bye bye");
         System.exit(0);
--- task02/src/Main.java
+++ task02/src/Main.java
@@ -7,34 +7,75 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
 
+        // accept input from user with args[0], in this case it will be "../texts"
         File directory = new File(args[0]);
         List<File> files = new ArrayList<>();
 
+        // call method to add files from given directory into List files
         addFiles(directory, files);
 
+        // new HashMaps to store word count values as well as next word values
         Map<String, Integer> wordCount = new HashMap<>();
         Map<String, Map<String, Integer>> nextWord = new HashMap<>();
 
+        // for each loop to read each file line by line
+        // line 35 will strip it of all punctuation and make every word lowercase, as
+        // well as split to a array
+        // line 37 is a for i loop that will go through the current word and next word
+        // in the array
+        // this is so we can update our word count and next word maps
         for (File file : files) {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = "";
+            String line;
 
             while ((line = br.readLine()) != null) {
                 String[] words = line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-                
 
+                for (int i = 0; i < words.length - 1; i++) {
+                    String currentWord = words[i];
+                    String next = words[i + 1];
 
+                    // update word count map
+                    wordCount.put(currentWord, wordCount.getOrDefault(currentWord, 0) + 1);
+
+                    // initialise new current word map
+                    // add next word info inside
+                    // update next word map 
+                    Map<String, Integer> currentWordMap = nextWord.getOrDefault(currentWord, new HashMap<>());
+                    currentWordMap.put(next, currentWordMap.getOrDefault(next, 0) + 1);
+                    nextWord.put(currentWord, currentWordMap);
+                }
+            }
+            // close bufferedreader to clear warning and release resources
+            br.close();
+        }
+
+        // another for each loop to go through the map and update total count
+        // probability is calculated by dividing frequency by total count
+        for (String word : nextWord.keySet()) {
+            Map<String, Integer> currentWordMap = nextWord.get(word);
+            int totalCount = wordCount.get(word);
+
+            for (String next : currentWordMap.keySet()) {
+                int frequency = currentWordMap.get(next);
+                double probability = (double) frequency / totalCount;
+
+                // then we will print each word
+                // followed by a new line with some spacing to follow sample output given in
+                // assessment paper
+                System.out.println(word);
+                System.out.println("    " + next + " " + String.format("%.2f", probability));
             }
         }
 
-        
-
-
     }
 
+    // method to add files into list to read from
+    // needed for calling the same method within a method to access subdirectories
+    // (recursive)
     public static void addFiles(File directory, List<File> files) {
         File[] fileList = directory.listFiles();
 
@@ -48,12 +89,5 @@ public class Main {
             }
         }
     }
-
-
-
-
-
-
-
 
 }
